3a52a5b3bee93eaec42649c2929e6cc1d5468155
pedromvba <pedromvba@gmail.com>
2024-09-05 14:36:25 -0300

    data transformation - chat gpt

---
+++ README.md
@@ -0,0 +1,2 @@
+Os dados foram processados pelo ChatGPT
+
---
+++ app/pages/1_Introduction.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Introduction",
+    page_icon="👋",
+)
+
+st.write('Página de Introdução')
+
+
